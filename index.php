4743e08479acde1cd8a630b49e05b4b7f703d146
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-02-01 21:04:24 +0300

    udt

--- index.php
+++ index.php
@@ -33,13 +33,13 @@ include 'init.php';
                         echo '<div class="date">' . $item['Add_Date'] . '</div>';
 
                         /// Add to Cart button (form)
-                        echo '<form action="cart.php" method="POST">';
+                        echo '<form action="" method="POST">';
                         echo '<input type="hidden" name="item_id" value="' . $item['Item_ID'] . '">';
                         echo '<input type="hidden" name="item_name" value="' . $item['Name'] . '">';
                         echo '<input type="hidden" name="item_price" value="' . $item['Price'] . '">';
                         echo '<button type="submit" class="btn btn-success add-to-cart">Add to Cart</button>';
                         echo '</form>';
-                        
+
                     echo '</div>';
                 echo '</div>';
             echo '</div>';
