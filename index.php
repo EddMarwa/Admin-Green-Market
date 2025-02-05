692415cc9e6b80e09e5ca70e79e4c5931d133081
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-02-05 14:09:33 +0300

    udt on checkout

--- index.php
+++ index.php
@@ -37,7 +37,9 @@ include 'init.php';
                             echo '<input type="hidden" name="item_id" value="' . $item['Item_ID'] . '">';
                             echo '<input type="hidden" name="item_name" value="' . $item['Name'] . '">';
                             echo '<input type="hidden" name="item_price" value="' . $item['Price'] . '">';
-                            echo '<button type="submit" class="btn btn-success add-to-cart">Proceed to Checkout</button>';
+                            //echo '<button type="submit" class="btn btn-success add-to-cart">Proceed to Checkout</button>';
+                            echo '<a href="checkout.php?item_id=' . $item['Item_ID'] . '&item_name=' . urlencode($item['Name']) . '&item_price=' . $item['Price'] . '" class="btn btn-success">Proceed to Checkout</a>';
+
                         echo '</form>';
                     echo '</div>';
                 echo '</div>';
