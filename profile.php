9d5e4569445682e1c97a11c95a710a064931b444
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-02-09 23:39:22 +0300

    styling

--- profile.php
+++ profile.php
@@ -54,12 +54,12 @@ if (isset($_SESSION['user'])) { // if there's a user stored in the Session, allo
                                                     echo '<span class="approve-status">Waiting approval</span>';
                                                 }
                                                 echo '<span class="price-tag"> Ksh ' . $item['Price'] . '</span>';
-                                                echo '<img class="img-responsive" src="img.jpg" alt="random image">';
+                                                echo '<img class="img-responsive" src="images/img.jpg" alt="random image">';
                                                 echo '<div class="caption">';
                                                     echo '<h3><a href="items.php?itemid=' . $item['Item_ID'] . '">' . $item['Name'] . '</a></h3>';
                                                     echo '<p>' . $item['Description'] . '</p>';
                                                     echo '<div class="date">' . $item['Add_Date'] . '</div>';
-                                                echo '</div>';
+                                                echo '</div>'; 
                                             echo '</div>';
                                         echo '</div>';
                                     }
