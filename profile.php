5b9dc46987fd50ceedfdc358adfb2d25e3dfa3d0
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-02-09 23:26:32 +0300

    price tag

--- profile.php
+++ profile.php
@@ -53,7 +53,7 @@ if (isset($_SESSION['user'])) { // if there's a user stored in the Session, allo
                                                 if ($item['Approve'] == 0) { // if the items is not approved yet
                                                     echo '<span class="approve-status">Waiting approval</span>';
                                                 }
-                                                echo '<span class="price-tag">$' . $item['Price'] . '</span>';
+                                                echo '<span class="price-tag"> Ksh ' . $item['Price'] . '</span>';
                                                 echo '<img class="img-responsive" src="img.jpg" alt="random image">';
                                                 echo '<div class="caption">';
                                                     echo '<h3><a href="items.php?itemid=' . $item['Item_ID'] . '">' . $item['Name'] . '</a></h3>';
