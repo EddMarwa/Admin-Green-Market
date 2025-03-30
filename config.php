8ce137ba43094b5277bffa6a18a1d7e928d3790c
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-03-30 10:25:20 +0300

    db querying

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ $conn = mysqli_connect("localhost", "root", "", "shop");
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
+
 ini_set('log_errors', 1);
 ini_set('error_log', 'errors.log');
 
