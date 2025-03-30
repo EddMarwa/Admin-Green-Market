eb0d12432afa725cfb58ba9542c9a4d157feafb8
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-03-30 10:25:09 +0300

    vdb querying

--- config.php
+++ config.php
@@ -5,7 +5,6 @@ $conn = mysqli_connect("localhost", "root", "", "shop");
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
-
 ini_set('log_errors', 1);
 ini_set('error_log', 'errors.log');
 
