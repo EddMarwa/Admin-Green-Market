c18e7cbb68797617878dfbec469c0cc02a2e9d1b
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-03-31 22:19:13 +0300

    update

--- admin/logout.php
+++ admin/logout.php
@@ -1,7 +1,6 @@
 <?php
 
 session_start();
-
 session_unset(); // Unsetting the data (or the session variable)
 session_destroy(); // Destroying the session
 echo "You have logged out and will be redirected after 3 secondes...";
