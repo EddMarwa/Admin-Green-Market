b648666b6d0fbb755f1e13f8750f6c47af6ab96a
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-04-05 17:12:04 +0300

    test message

--- admin/logout.php
+++ admin/logout.php
@@ -3,7 +3,7 @@
 session_start();
 session_unset(); // Unsetting the data (or the session variable)
 session_destroy(); // Destroying the session
-echo "You have logged out and will be redirected after 3 secondes...";
+echo "You have logged out and will be redirected after 3 seconds...";
 //header('Location: index.php');
 header('REFRESH:3; URL=index.php'); // to be headed after a certain time duration you exactly want    // Redirect to eCommerce\admin\index.php
 exit();
