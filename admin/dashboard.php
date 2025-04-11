7f8819e4747965ee98e88f5093980cec1df52135
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-04-11 07:38:53 +0300

    Gm update

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -20,7 +20,6 @@ if (isset($_SESSION['Username'])) { // if there's a user stored in the Session, 
         $latestItems = getLatest('*', 'items', 'Item_ID', $numItems); // Latest Items Array
         $numComments = 5; // The number of latest comments
         $latestComments = getLatest('*', 'items', 'Item_ID', $numItems); // Latest Items Array
-
     ?>
         <div class="home-stats">
             <div class="container text-center"><!--class home--stats will take any CSS edits but will be used only as a parent-->
